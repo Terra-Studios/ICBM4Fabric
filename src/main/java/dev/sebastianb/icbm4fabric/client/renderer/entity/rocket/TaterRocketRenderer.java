17b0ee74194618a8d13ed4b547c6fa43f2981216
SebaSphere <SebaSphereEmails@gmail.com>
2021-03-09 11:53:28 -0500

    CHORE: Trying to get tater missile working. Ticking entity crash on summon.

--- src/main/java/dev/sebastianb/icbm4fabric/client/renderer/entity/rocket/TaterRocketRenderer.java
+++ src/main/java/dev/sebastianb/icbm4fabric/client/renderer/entity/rocket/TaterRocketRenderer.java
@@ -5,11 +5,12 @@ import dev.sebastianb.icbm4fabric.client.model.entity.rocket.TaterRocketModel;
 import dev.sebastianb.icbm4fabric.entity.rocket.TaterRocketEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
-public class TaterRocketRenderer extends MobEntityRenderer<TaterRocketEntity, TaterRocketModel> {
+public class TaterRocketRenderer extends LivingEntityRenderer<TaterRocketEntity, TaterRocketModel> {
 
 
     public TaterRocketRenderer(EntityRenderDispatcher dispatcher) {
--- src/main/java/dev/sebastianb/icbm4fabric/entity/rocket/TaterRocketEntity.java
+++ src/main/java/dev/sebastianb/icbm4fabric/entity/rocket/TaterRocketEntity.java
@@ -5,16 +5,17 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
 import net.minecraft.util.Arm;
 import net.minecraft.world.World;
 
-public class TaterRocketEntity extends MobEntity {
+public class TaterRocketEntity extends LivingEntity {
 
 
-    public TaterRocketEntity(EntityType<? extends MobEntity> entityType, World world) {
+    public TaterRocketEntity(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
 
