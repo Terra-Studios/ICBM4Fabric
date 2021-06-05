7b1c8f2df3f99eee6056364fd7702e83fb6740b0
SebaSphere <SebaSphereEmails@gmail.com>
2021-06-05 19:59:17 -0400

    FEAT: Add texture for missile launcher screen

--- src/main/java/dev/sebastianb/icbm4fabric/client/gui/LaunchScreen.java
+++ src/main/java/dev/sebastianb/icbm4fabric/client/gui/LaunchScreen.java
@@ -5,20 +5,28 @@ import dev.sebastianb.icbm4fabric.Constants;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.ScreenTexts;
+import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.client.gui.screen.ingame.CraftingScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+@SuppressWarnings("all") // like every good programmer
 @Environment(EnvType.CLIENT)
 public class LaunchScreen extends HandledScreen<LaunchScreenHandler> {
 
 
-    private static final Identifier TEXTURE = new Identifier(Constants.MOD_ID, "textures/gui/frog.png"); // placeholder
+    private static final Identifier TEXTURE = new Identifier(Constants.MOD_ID, "textures/gui/missile_launcher_screen.png");
+    private final int textureWidth = 256;
+    private final int textureHeight = 256;
+    private float textureScale = 0.87f;
 
 
     public LaunchScreen(LaunchScreenHandler handler, PlayerInventory inventory, Text title) {
@@ -27,18 +35,32 @@ public class LaunchScreen extends HandledScreen<LaunchScreenHandler> {
 
     @Override
     protected void init() {
+        super.backgroundWidth = textureWidth;
+        super.backgroundHeight = textureHeight;
+
         super.init();
-        this.addButton();
+        // this.addButton();
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        // RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        int x = (width - backgroundWidth) / 2;
-        int y = (height - backgroundHeight) / 2;
-        drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
-        super.render(matrices,mouseX,mouseY,delta);
+        this.renderBackground(matrices);
+        matrices.push(); {
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+            RenderSystem.setShaderTexture(0, TEXTURE);
+
+            int x = (int) ((width / 2) - ((backgroundWidth / 2) * textureScale));
+            int y = (int) ((height/ 2) - ((backgroundHeight / 2) * textureScale));
+
+            matrices.translate(x, y, 0);
+            matrices.scale(textureScale, textureScale, textureScale);
+
+            drawTexture(matrices, 0, 0, 0, 0, backgroundWidth, backgroundHeight, 256, 256);
+
+
+        }
+        matrices.pop();
 
     }
 
@@ -52,8 +74,7 @@ public class LaunchScreen extends HandledScreen<LaunchScreenHandler> {
         return false;
     }
 
-    // doesn't seem to be displaying
-    // TODO: I forgot what this does but update to 1.17? OH it's the button on it?
+
     private void addButton() {
         this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, 196, 200, 20, ScreenTexts.YES, (buttonWidget) -> {
             this.client.openScreen(null);
