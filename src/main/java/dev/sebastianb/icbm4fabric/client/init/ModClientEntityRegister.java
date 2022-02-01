e568c80070f4995b3e2df24a779905425438e583
Atakku <atakkudev@gmail.com>
2022-02-01 01:05:23 +0100

    fix: WHO THE FUCK SUPPRESSED DEPRECATION WARNINGS??????

--- src/main/java/dev/sebastianb/icbm4fabric/client/init/ModClientEntityRegister.java
+++ src/main/java/dev/sebastianb/icbm4fabric/client/init/ModClientEntityRegister.java
@@ -4,13 +4,12 @@ import dev.sebastianb.icbm4fabric.client.Icbm4fabricClient;
 import dev.sebastianb.icbm4fabric.client.model.entity.missile.TaterMissileModel;
 import dev.sebastianb.icbm4fabric.client.renderer.entity.missile.TaterMissileRenderer;
 import dev.sebastianb.icbm4fabric.entity.ModEntityTypes;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
-@SuppressWarnings("all") // like every good programmer
 public class ModClientEntityRegister {
     public static void register() {
-        EntityRendererRegistry.INSTANCE.register(ModEntityTypes.TATER_MISSILE, TaterMissileRenderer::new);
+        EntityRendererRegistry.register(ModEntityTypes.TATER_MISSILE, TaterMissileRenderer::new);
 
         EntityModelLayerRegistry.registerModelLayer(Icbm4fabricClient.TATER_LAYER, TaterMissileModel::getTextureModelData);
     }
