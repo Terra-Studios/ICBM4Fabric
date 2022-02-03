9b7d60b9821a8d1627da2b997d2950086afa29e8
BigSpaceships <bigspaceships56@gmail.com>
2022-02-02 20:35:36 -0500

    FEAT: merged/added launch paths maybe?

--- src/main/java/dev/sebastianb/icbm4fabric/entity/missile/path/AbstractLaunchPath.java
+++ src/main/java/dev/sebastianb/icbm4fabric/entity/missile/path/AbstractLaunchPath.java
@@ -1,6 +1,9 @@
 package dev.sebastianb.icbm4fabric.entity.missile.path;
 
 import dev.sebastianb.icbm4fabric.entity.missile.AbstractMissileProjectile;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 public abstract class AbstractLaunchPath {
     AbstractMissileProjectile rocket;
@@ -11,25 +14,25 @@ public abstract class AbstractLaunchPath {
     public abstract void updateMotion();
 
     public void updateRotation() {
-        double vX = rocket.vX;
-        double vY = rocket.vY;
-        double vZ = rocket.vZ;
-        if (vX == 0 && vY == 0 && vZ == 0) // could be better
-            return;
-
-        // below is in radians
-        double yaw = Math.atan2(vX, vZ);
-        double pitch = Math.atan2(Math.sqrt(Math.pow(vX, 2) + Math.pow(vZ, 2)), vY);
-
-        // radians to degrees
-        float realYaw = (float) Math.toDegrees(yaw);
-        float realPitch = (float) Math.toDegrees(pitch);
-
-        if (Double.isNaN(realYaw) || Double.isNaN(realPitch)) {
-            rocket.setRotation(rocket.prevYaw, rocket.prevPitch); // (To prevent) Invalid entity rotation: NaN, discarding.
+        Vec3d vec3d = rocket.getVelocity();
+        if (Double.isNaN(vec3d.x) || Double.isNaN(vec3d.y) || Double.isNaN(vec3d.z)) {
             return;
         }
-        rocket.setRotation(realYaw, realPitch);
+        if (rocket.prevPitch == 0.0f && rocket.prevYaw == 0.0f) {
+            double d = vec3d.horizontalLength();
+            rocket.setYaw((float) (MathHelper.atan2(vec3d.x, vec3d.z) * 57.2957763671875));
+            rocket.setPitch((float) (MathHelper.atan2(vec3d.y, d) * 57.2957763671875));
+            rocket.prevYaw = rocket.getYaw();
+            rocket.prevPitch = rocket.getPitch();
+        }
+        double e = vec3d.x;
+        double f = vec3d.y;
+        double g = vec3d.z;
+        double l = vec3d.horizontalLength();
+        rocket.setYaw((float) (MathHelper.atan2(e, g) * 57.2957763671875));
+        rocket.setPitch((float) (MathHelper.atan2(f, l) * 57.2957763671875));
+        rocket.setPitch(PersistentProjectileEntity.updateRotation(rocket.prevPitch, rocket.getPitch()));
+        rocket.setYaw(PersistentProjectileEntity.updateRotation(rocket.prevYaw, rocket.getYaw()));
 
     }
 }
